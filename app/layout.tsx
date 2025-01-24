c4f9bfd0e870075df6997335b8cf939084e5abc4
Aidan <aidan@p0ntus.com>
2025-01-23 19:35:51 -0500

    add favicon, metadata in layout template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css"
 import type { Metadata } from "next"
+import Head from "next/head"
 import { Inter } from "next/font/google"
 import Navbar from "../app/components/Navbar"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "LibreCloud - Free Cloud Services",
+  title: "LibreCloud",
   description: "Secure and private cloud services including email, password management, and code hosting.",
 }
 
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{`${metadata.title}`}</title>
+        <meta name="description" content={metadata.description ?? ''} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={`${inter.className} bg-gray-900 text-gray-100`}>
         <Navbar />
         {children}
